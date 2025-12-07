8aefd822b427a0332fbb56239c6a26298df5cceb
Hajdú Patrik Zsolt <hajdu.patrik1023@gmail.com>
2025-12-07 15:36:30 +0100

    Threads on again

--- app/src/main/java/networkmonitor/db/BlacklistFetching.java
+++ app/src/main/java/networkmonitor/db/BlacklistFetching.java
@@ -49,7 +49,7 @@ public class BlacklistFetching implements Runnable {
         loadManualList(loader, dao);
 
         // 3. Incremental load
-        loadIncrementalWebList(loader, dao, 2040);
+        loadIncrementalWebList(loader, dao, 5000);
     }
 
     /**
